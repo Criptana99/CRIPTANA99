ebe17b90dfb1b93ef72dd03a183fa45b6b843fba
Criptana99 <criptana@gmail.com>
2024-03-06 17:56:01 +0100

    1º commit de la clase Main

---
+++ Tarea4Endes24/src/tarea4endes24/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tarea4endes24;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta miCuenta;
+        double saldoActual;
+        
+
+        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = miCuenta.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+        
+    
+    }
+
+    /* añado un constructor vacio*/
+    public Main() {
+    }
+    
+    public static void probarIngresar(CCuenta c, int cantidad) throws Exception{
+        c.ingresar(cantidad);
+        
+    }
+    
+    public static void probarRetirar(CCuenta c, int cantidad) throws Exception{
+        c.retirar(cantidad);
+    }
+}
